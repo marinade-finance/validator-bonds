03bab10c2d37b28d08dc932b186aa3ab64d82977
Ondra Chaloupka <chalda@marinade.finance>
2024-05-09 22:46:59 +0200

    dependency update + fixes

--- scripts/settlement-json-listing.sh
+++ scripts/settlement-json-listing.sh
@@ -4,11 +4,30 @@
 # settlement-json-listing.sh ""$JSON_FILE""
 ### ----
 
+solsdecimal() {
+  N="$@"
+  if [ ${#N} -lt 9 ]; then
+    echo ".${N}"
+  else
+    SOLS="${N::-9}"
+    echo "${SOLS:-0}.${N:${#SOLS}}"
+  fi
+}
+
+
 JSON_FILE="$1"
+
+# 272 bytes
+CLAIM_ACCOUNT_DATA_RENT=0.002784
 
 # sum of max total claim from json
 echo -n "Sum of max total claim at '$JSON_FILE': "
-jq '.merkle_trees[].max_total_claim_sum' "$JSON_FILE" | paste -s -d+ | bc
+LAMPORTS=$(jq '.merkle_trees[].max_total_claim_sum' "$JSON_FILE" | paste -s -d+ | bc)
+solsdecimal $LAMPORTS
+echo -n "Number of all claims in file + expected rent: "
+NUMBER_OF_CLAIMS=$(jq '.merkle_trees[].tree_nodes | length'  "$JSON_FILE" |  paste -s -d+ | bc)
+RENT=$(echo "scale=4; $NUMBER_OF_CLAIMS * $CLAIM_ACCOUNT_DATA_RENT" | bc)
+echo "$NUMBER_OF_CLAIMS, expected rent for newly created: $RENT"
 
 echo 'Data of vote account and max total sum claim:'
 grep "$JSON_FILE" -e 'vote_account' -e 'max_total_claim_sum'
@@ -17,20 +36,21 @@ grep "$JSON_FILE" -e 'vote_account' -e 'max_total_claim_sum'
 # jq '.merkle_trees[] | {sum: .max_total_claim_sum, vote_account: .vote_account, claims: [.tree_nodes[].claim]}' "$JSON_FILE"
 
 # number of claims for a vote account
-FILE="./$JSON_FILE"
-COUNT=$(jq '.merkle_trees | length'  "$FILE")
-echo "Number of merkle trees: $COUNT at $FILE"
+COUNT=$(jq '.merkle_trees | length'  "$JSON_FILE")
+echo "Number of merkle trees: $COUNT at $JSON_FILE"
 for I in $(seq 0 $((COUNT-1)) ); do
   echo "Index: $I"
   echo -n 'Vote account: '
-  jq   ".merkle_trees[$I] | .vote_account" "$FILE"
+  jq   ".merkle_trees[$I] | .vote_account" "$JSON_FILE"
   echo -n 'Max claim sum: '
-  jq   ".merkle_trees[$I] | .max_total_claim_sum" "$FILE"
-  echo -n '# of claims: '
-  jq   ".merkle_trees[$I] | .tree_nodes | length" "$FILE"
+  LAMPORTS=$(jq   ".merkle_trees[$I] | .max_total_claim_sum" "$JSON_FILE")
+  solsdecimal $LAMPORTS
+  echo -n 'Number of claims: '
+  jq   ".merkle_trees[$I] | .tree_nodes | length" "$JSON_FILE"
   echo -n 'Claims sum: '
-  jq ".merkle_trees[$I] | .tree_nodes[].claim" "$FILE" | paste -s -d+ | bc
-  echo
+  LAMPORTS=$(jq ".merkle_trees[$I] | .tree_nodes[].claim" "$JSON_FILE" | paste -s -d+ | bc)
+  solsdecimal $LAMPORTS
+  echo '----------------'
 done
 
 # TODO: utilize nodejs CLI to get the data
