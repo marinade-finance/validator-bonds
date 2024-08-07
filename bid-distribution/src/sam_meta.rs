fca6bb2c8076fa3307ab6d0d36d60c9ed9aecf36
TWloYWkgQm9kbmFyaXU= <mihai.bodnariu@butonium.com>
2024-08-08 00:38:20 +0300

    fix: parsing

--- bid-distribution/src/sam_meta.rs
+++ bid-distribution/src/sam_meta.rs
@@ -28,34 +28,15 @@ pub struct Metadata {
 pub struct ValidatorSamMeta {
     #[serde(with = "pubkey_string_conversion")]
     pub vote_account: Pubkey,
-    #[serde(deserialize_with = "string_to_f64", serialize_with = "f64_to_string")]
     pub marinade_mnde_target_sol: f64,
-    #[serde(deserialize_with = "string_to_f64", serialize_with = "f64_to_string")]
     pub marinade_sam_target_sol: f64,
     pub rev_share: String,
     pub stake_priority: u32,
     pub unstake_priority: u32,
-    #[serde(deserialize_with = "string_to_f64", serialize_with = "f64_to_string")]
     pub max_stake_wanted: f64,
-    #[serde(deserialize_with = "string_to_f64", serialize_with = "f64_to_string")]
     pub effective_bid: f64,
     pub constraints: String,
     pub metadata: Metadata,
     pub scoring_run_id: String,
     pub epoch: u32,
-}
-
-fn string_to_f64<'de, D>(deserializer: D) -> Result<f64, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    s.parse::<f64>().map_err(serde::de::Error::custom)
-}
-
-fn f64_to_string<S>(x: &f64, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    serializer.serialize_str(&x.to_string())
 }
