819d18fdb44d37451e5a4d8ca4b8d628c53e5171
Ondra Chaloupka <chalda@marinade.finance>
2024-09-05 16:51:15 +0200

    [scripts] speeding up script processing

--- scripts/settlement-json-listing.sh
+++ scripts/settlement-json-listing.sh
@@ -34,22 +34,24 @@ fi
 # stake account minimal size (1 SOL is hardcoded here but can be dfferent based on Config)
 STAKE_ACCOUNT_MINIMAL_SIZE=$((1000000000 + 2282880))
 
-
 SETTLEMENTS_EPOCH=$(jq '.epoch' "$SETTLEMENTS_JSON_FILE")
 MERKLE_TREES_EPOCH=$(jq '.epoch' "$MERKLE_TREES_JSON_FILE")
 if [ "$SETTLEMENTS_EPOCH" != "$MERKLE_TREES_EPOCH" ]; then
     echo "Epochs of files '$SETTLEMENTS_JSON_FILE' and '$MERKLE_TREES_JSON_FILE' are not matching: Settlements epoch: $SETTLEMENTS_EPOCH, Merkle trees epoch: $MERKLE_TREES_EPOCH"
     exit 1
 fi
+echo "EPOCH: $SETTLEMENTS_EPOCH"
+
+# Preload the JSON data into a variable to avoid reading from the file multiple times
+merkle_trees=$(jq -c '.merkle_trees[]' "$MERKLE_TREES_JSON_FILE")
 
 # sum of max total claim from json
-echo "EPOCH: $SETTLEMENTS_EPOCH"
 echo -n "Sum of max total claim at '$MERKLE_TREES_JSON_FILE': "
-LAMPORTS=$(jq '.merkle_trees[].max_total_claim_sum' "$MERKLE_TREES_JSON_FILE" | paste -s -d+ | bc)
+LAMPORTS=$(echo "$merkle_trees" | jq -r '.max_total_claim_sum' | paste -s -d+ | bc)
 solsdecimal $LAMPORTS
-NUMBER_OF_CLAIMS=$(jq '.merkle_trees[].tree_nodes | length'  "$MERKLE_TREES_JSON_FILE" |  paste -s -d+ | bc)
+NUMBER_OF_CLAIMS=$(echo "$merkle_trees" | jq -r '.tree_nodes | length' |  paste -s -d+ | bc)
 echo "Number of all claims: $NUMBER_OF_CLAIMS"
-COUNT=$(jq '.merkle_trees | length'  "$MERKLE_TREES_JSON_FILE")
+COUNT=$(echo "$merkle_trees" | wc -l)
 echo "Number of merkle trees: $COUNT"
 echo '----------------'
 
@@ -58,21 +60,28 @@ echo '----------------'
 # grep "$MERKLE_TREES_JSON_FILE" -e 'vote_account' -e 'max_total_claim_sum'
 # jq '.merkle_trees[] | {sum: .max_total_claim_sum, vote_account: .vote_account, claims: [.tree_nodes[].claim]}' "$MERKLE_TREES_JSON_FILE"
 
+settlements=$(jq -c '.settlements[]' "$SETTLEMENTS_JSON_FILE")
+
 declare -A claims_amounts
 declare -A claims_number
 
-for I in $(seq 0 $((COUNT-1)) ); do
-  echo "Index: $I"
-  VOTE_ACCOUNT=$(jq   ".merkle_trees[$I] | .vote_account" "$MERKLE_TREES_JSON_FILE")
+index=0
+while IFS= read -r tree; do
+  echo "Index: $index"
+  VOTE_ACCOUNT=$(echo "$tree" | jq -r '.vote_account')
+  LAMPORTS_MAX=$(echo "$tree" | jq -r '.max_total_claim_sum')
+  LAMPORTS_SUM=$(echo "$tree" | jq '.tree_nodes[].claim' | paste -s -d+ | bc)
+  CLAIMS_SUM=$(solsdecimal "$LAMPORTS_SUM")
+  MAX_CLAIM_SUM=$(solsdecimal "$LAMPORTS_MAX")
+
   echo "Vote account: $VOTE_ACCOUNT"
-  LAMPORTS_MAX=$(jq   ".merkle_trees[$I] | .max_total_claim_sum" "$MERKLE_TREES_JSON_FILE")
-  MAX_CLAIM_SUM=$(solsdecimal $LAMPORTS_MAX)
-  LAMPORTS_SUM=$(jq ".merkle_trees[$I] | .tree_nodes[].claim" "$MERKLE_TREES_JSON_FILE" | paste -s -d+ | bc)
-  CLAIMS_SUM=$(solsdecimal $LAMPORTS_SUM)
   echo "Max claim sum/Claims sum: ${MAX_CLAIM_SUM}/${CLAIMS_SUM}"
+
   echo -n 'Number of claims: '
-  jq ".merkle_trees[$I] | .tree_nodes | length" "$MERKLE_TREES_JSON_FILE"
-  FUNDER=$(jq -c '.settlements[] | select ((.vote_account == '$VOTE_ACCOUNT') and (.claims_amount == '$LAMPORTS_MAX')) | .meta.funder' "$SETTLEMENTS_JSON_FILE")
+  echo "$tree" | jq '.tree_nodes | length'
+
+  # Query the settlement data once per loop
+  FUNDER=$(echo "$settlements" | jq -c 'select((.vote_account == "'$VOTE_ACCOUNT'") and (.claims_amount == '$LAMPORTS_MAX')) | .meta.funder')
   echo "Funder: ${FUNDER:-<UNKNOWN>}"
 
   current_sum=${claims_amounts[$FUNDER]}
@@ -80,7 +89,8 @@ for I in $(seq 0 $((COUNT-1)) ); do
   current_number=${claims_number[$FUNDER]}
   claims_number[$FUNDER]=$((current_number+1))
   echo '----------------'
-done
+  index=$((index + 1))
+done <<< "$merkle_trees"
 
 echo
 echo '========================='
