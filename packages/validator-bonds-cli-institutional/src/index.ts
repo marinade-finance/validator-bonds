907c69593940ba36666928f79460449a532399fa
chalda <chalda@marinade.finance>
2025-09-03 15:59:57 +0200

    CLI: permit validator identity to be used when showing bond info [GEN-6189] (#268)
    * [cli] check for identity when search address
    
    * [versions] bump to 2.1.10
    
    * [review] fixes

--- packages/validator-bonds-cli-institutional/src/index.ts
+++ packages/validator-bonds-cli-institutional/src/index.ts
@@ -8,7 +8,7 @@ export const VALIDATOR_BONDS_NPM_URL =
   'https://registry.npmjs.org/@marinade.finance/validator-bonds-cli-institutional'
 
 launchCliProgram({
-  version: '2.1.9',
+  version: '2.1.10',
   installAdditionalOptions: program => {
     program.setOptionValueWithSource(
       'programId',
