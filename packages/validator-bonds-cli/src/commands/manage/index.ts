5da2fdd97bcf9c9995aeed0ab901dc339360ab98
Ondra Chaloupka <chalda@marinade.finance>
2026-02-27 16:34:36 +0100

    [cli] subscription implemenation

--- packages/validator-bonds-cli/src/commands/manage/index.ts
+++ packages/validator-bonds-cli/src/commands/manage/index.ts
@@ -15,6 +15,9 @@ import { installInitWithdrawRequest } from './initWithdrawRequest'
 import { installStakeMerge } from './mergeStake'
 import { installMintBond } from './mintBond'
 import { installResetStake } from './resetStake'
+import { installSubscribe } from './subscribe'
+import { installSubscriptions } from './subscriptions'
+import { installUnsubscribe } from './unsubscribe'
 
 import type { Command } from 'commander'
 
@@ -34,4 +37,7 @@ export function installManage(program: Command) {
   installEmergencyResume(program)
   installCloseSettlement(program)
   installResetStake(program)
+  installSubscribe(program)
+  installUnsubscribe(program)
+  installSubscriptions(program)
 }
