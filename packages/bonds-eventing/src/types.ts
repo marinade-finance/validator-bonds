3c840dc61fe8e9d34ee688806efad13246432e5b
Ondra Chaloupka <chalda@marinade.finance>
2026-04-09 20:41:00 +0200

    [events] better type for validator is out of auction

--- packages/bonds-eventing/src/types.ts
+++ packages/bonds-eventing/src/types.ts
@@ -2,7 +2,7 @@ export type {
   BondsEventV1,
   BondsEventInnerType,
   FirstSeenDetails,
-  BondRemovedDetails,
+  ValidatorDelistedDetails,
   AuctionEnteredDetails,
   AuctionExitedDetails,
   CapChangedDetails,
