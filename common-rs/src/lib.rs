da5f29b23df593f7a7635e5521ec4b1ad17f761c
Ondra Chaloupka <chalda@marinade.finance>
2026-07-15 17:16:39 +0200

    [clippy] fix

--- common-rs/src/lib.rs
+++ common-rs/src/lib.rs
@@ -24,6 +24,8 @@ pub fn get_validator_bonds_program(
     rpc_client: Arc<RpcClient>,
     payer: Option<Arc<DynSigner>>,
 ) -> anyhow::Result<Program<Arc<DynSigner>>> {
+    // anchor-client's Program/Client API dictates the Arc<DynSigner> handle type
+    #[allow(clippy::arc_with_non_send_sync)]
     let payer = payer.unwrap_or(Arc::new(DynSigner(Arc::new(Keypair::new()))));
 
     Ok(Client::new_with_options(
