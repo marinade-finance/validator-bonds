5f4bd6bd22264ffb422856b8111e6b97ad4d4c97
Ondra Chaloupka <chalda@marinade.finance>
2026-09-04 00:14:10 +0200

    [scripts] update version update

--- scripts/update-cli-version.sh
+++ scripts/update-cli-version.sh
@@ -6,11 +6,13 @@
 SCRIPT_PATH=`readlink -f "$0"`
 SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
 
-CLI_INDEX="$SCRIPT_DIR/../packages/validator-bonds-cli/src/index.ts"
-CLI_INSTITUTIONAL_INDEX="$SCRIPT_DIR/../packages/validator-bonds-cli-institutional/src/index.ts"
-README="$SCRIPT_DIR/../README.md"
-README_CLI="$SCRIPT_DIR/../packages/validator-bonds-cli/README.md"
-README_INSTITUTIONAL_CLI="$SCRIPT_DIR/../packages/validator-bonds-cli-institutional/README.md"
+VERSION_FILES=(
+  "$SCRIPT_DIR/../packages/validator-bonds-cli/src/index.ts"
+  "$SCRIPT_DIR/../packages/validator-bonds-cli-institutional/src/index.ts"
+  "$SCRIPT_DIR/../README.md"
+  "$SCRIPT_DIR/../packages/validator-bonds-cli/README.md"
+  "$SCRIPT_DIR/../packages/validator-bonds-cli-institutional/README.md"
+)
 
 SDK_PACKAGE_JSON="$SCRIPT_DIR/../packages/validator-bonds-sdk/package.json"
 [ ! -f "$SDK_PACKAGE_JSON" ] && echo "$SDK_PACKAGE_JSON not found" && exit 1
@@ -34,7 +36,7 @@ done
 
 
 echo "$PREVIOUS_VERSION -> $UPDATE_NEW_VERSION"
-for I in "$CLI_INDEX" "$CLI_INSTITUTIONAL_INDEX" "$README" "$README_CLI" "$README_INSTITUTIONAL_CLI"; do
+for I in "${VERSION_FILES[@]}"; do
     UPDATE_FILE=`readlink -f "$I"`
     echo "Updating ${UPDATE_FILE}"
     sed -i "s/$PREVIOUS_ESCAPED_VERSION/$UPDATE_NEW_VERSION/" "$UPDATE_FILE"
