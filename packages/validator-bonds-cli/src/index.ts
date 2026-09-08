12b8adf53384a16b4dbc462e726b80c18c655b66
Ondra Chaloupka <chalda@marinade.finance>
2026-09-08 17:15:33 +0200

    [version] bump 2.6.3

--- packages/validator-bonds-cli/src/index.ts
+++ packages/validator-bonds-cli/src/index.ts
@@ -13,7 +13,7 @@ export const VALIDATOR_BONDS_NPM_URL =
   'https://registry.npmjs.org/@marinade.finance/validator-bonds-cli'
 
 launchCliProgram({
-  version: '2.6.2',
+  version: '2.6.3',
   installAdditionalOptions: program => {
     program.option(
       '--program-id <pubkey>',
